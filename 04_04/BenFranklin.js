73dbbb1dbc086e5a7a03d7b550b35b96353103a4
Jessica Blevins <jessica.k.blevins@gmail.com>
2018-07-19 13:57:00 -0400

    learning to export custom modules

--- 04_04/BenFranklin.js
+++ 04_04/BenFranklin.js
@@ -1,25 +1,20 @@
-// Handling pub/sub events with EventEmitter
-// https://nodejs.org/api/events.html#events_class_eventemitter
-var EventEmitter = require('events').EventEmitter;
-var util = require('util');
-
-var Person = function(name) {
-	this.name = name;
-};
-
-// The Person object inherits all of the functionality
-// of the EventEmitter.
-// e.g. a Person object will have an on() function for
-// the event listener
-util.inherits(Person, EventEmitter);
+var Person = require("./lib/Person");
 
 var ben = new Person("Ben Franklin");
+var george = new Person("George Washington");
 
 // event listener for a 'speak' event
 ben.on('speak', function(said) {
 	console.log(`${this.name}: ${said}`);
 });
 
+// event listener for George instance
+george.on('speak', function(said) {
+	console.log(`${this.name} -> ${said}`);
+});
+
 // Any listeners listening for the speak event
 // will fire their custom callbacks
 ben.emit('speak', 'You may delay, but time will not.');
+
+george.emit('speak', 'It is far better to be alone, than to be in bad company.');
