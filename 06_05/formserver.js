696a6ada5e648e57c7342369ad9809b98dbdb8cd
Jessica Blevins <jessica.k.blevins@gmail.com>
2018-07-21 19:16:03 -0400

    collecting POST data via the HTTP module

---
+++ 06_05/formserver.js
@@ -0,0 +1,40 @@
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function(request, response) {
+
+	if (request.method === "GET") {
+		response.writeHead(200, {"Content-Type": "text/html"});
+
+		fs.createReadStream("./public/form.html", "UTF-8").pipe(response);		
+	}
+	else if (request.method === "POST") {
+		var body = "";
+
+		request.on("data", function (chunk) {
+			body += chunk;
+		});
+
+		request.on("end", function() {
+			response.writeHead(200, {"Content-Type": "text/html"});
+
+			response.end(`
+				<!DOCTYPE html>
+				<html>
+					<head>
+						<title>Form Results</title>
+					</head>
+					<body>
+						<h1>Your Form Results</h1>
+						<p>${body}</p>
+					</body>
+				</html>
+			`);
+		});
+	}
+
+
+
+}).listen(3000);
+
+console.log("Server listening on port 3000");
