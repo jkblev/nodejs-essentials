53cb3645b4b6d1dfb458847e83905caad9f94cf8
Jessica Blevins <jessica.k.blevins@gmail.com>
2018-07-22 19:10:47 -0400

    express routing of dictionary api

--- 08_03/app.js
+++ 08_03/app.js
@@ -2,6 +2,20 @@ var express = require("express");
 
 var app = express();
 
+var skierTerms = [
+	{
+		term: "Rip",
+		defined: "To move at a high rate of speed"
+	},
+	{
+		term: "Huck",
+		defined: "To throw your body off of something, usually a natural feature like a cliff"
+	},
+	{
+		term: "Chowder",
+		defined: "Powder after it has been sufficiently skied"
+	}
+];
 
 app.use(function(req, res, next) {
 	console.log(`${req.method} request for '${req.url}'`);
@@ -9,6 +23,10 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static("./public"));
+
+app.get("/dictionary-api", function(request, response) {
+	response.json(skierTerms);
+});
 
 
 app.listen(3000);
